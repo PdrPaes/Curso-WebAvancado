b74a87876495513e8cc0e263c9d98976b2c9dff9
Pedro Henrique Paes <phpaes252@gmail.com>
2021-03-09 20:35:08 -0300

    commit for update

--- JavaScript/funcao/arrowFunction1.js
+++ JavaScript/funcao/arrowFunction1.js
@@ -7,3 +7,11 @@ dobro = (a) => {
 }
 
 dobro = a => 2*a
+console.log(dobro(Math.PI))
+
+let ola = function () {
+    return "Olá"
+}
+ola = () => "Olá"
+ola = _ => "Olá" // possui um param
+console.log(ola())
