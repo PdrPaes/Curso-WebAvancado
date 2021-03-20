af9b72aa19acee8bfcc2b96bf9ace49cf997d7b4
Pedro Henrique Paes <phpaes252@gmail.com>
2021-03-20 18:14:29 -0300

    :rocket:

---
+++ JavaScript/exercicios/exercicio03.js
@@ -0,0 +1,6 @@
+//03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+let expoente = function(a, b){
+    resp = a ** b
+    return resp
+}
+console.log(expoente(3,4))
