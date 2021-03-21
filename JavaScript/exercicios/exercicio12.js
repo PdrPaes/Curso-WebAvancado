42f27606b9b5ee018b58a8ff3fc36ff89d2dcdb2
Pedro Henrique Paes <phpaes252@gmail.com>
2021-03-21 01:09:30 -0300

    Commit for update :rocket:

---
+++ JavaScript/exercicios/exercicio12.js
@@ -0,0 +1,9 @@
+/*12) Faça um algoritmo que calcule o fatorial de um número.*/
+let fatorial = function(a){
+    contador1 = a
+    for(let i=1; contador1>0; i++){
+
+    }
+
+}
+console.log(fatorial(8))
