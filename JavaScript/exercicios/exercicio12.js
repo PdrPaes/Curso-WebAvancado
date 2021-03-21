905c2ccee80f7ab85494945d85ec8def9f41d257
Pedro Henrique Paes <phpaes252@gmail.com>
2021-03-21 20:06:53 -0300

    :rocket:

--- JavaScript/exercicios/exercicio12.js
+++ JavaScript/exercicios/exercicio12.js
@@ -1,9 +1,13 @@
 /*12) Faça um algoritmo que calcule o fatorial de um número.*/
 let fatorial = function(a){
     contador1 = a
-    for(let i=1; contador1>0; i++){
-
+    for(let i=1; contador1>1; i++){
+        contador1--
+        //console.log(contador1)
+        a = a * contador1
+        //console.log(a)
     }
-
+    return a
 }
+
 console.log(fatorial(8))
