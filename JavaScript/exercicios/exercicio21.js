168b3a76457e37a1f66b699f4979be887433dd91
Pedro Henrique Paes <phpaes252@gmail.com>
2021-03-22 10:54:28 -0300

    :rocket:

--- JavaScript/exercicios/exercicio21.js
+++ JavaScript/exercicios/exercicio21.js
@@ -2,4 +2,19 @@
 considerando que todos pagam R$ 100 mais um adicional conforme a seguinte tabela: 1) crianças com menos
 de 10 anos pagam R$80; 2) conveniados com idade entre 10 e 30 anos pagam R$50; 3) conveniados com
 idade acima de 30 e até 60 anos pagam R$ 95; e 4) conveniados acima de 60 anos pagam R$130*/
-let 
+let planoSaude = function(a){
+    if(a < 10){
+        preco = 180
+    } else if(a < 30) {
+        preco = 150
+    } else if (a < 60) {
+        preco = 195
+    } else {
+        preco = 230
+    }
+    return preco
+}
+console.log(planoSaude(9))
+console.log(planoSaude(19))
+console.log(planoSaude(39))
+console.log(planoSaude(90))
