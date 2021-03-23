82a7a8237b88d2653c581b59eee96db792d6dfb0
Pedro Henrique Paes <phpaes252@gmail.com>
2021-03-23 09:26:30 -0300

    :rocket:

---
+++ JavaScript/exercicios/exercicio24.js
@@ -0,0 +1,8 @@
+/*24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.*/
+a = 11
+while(a<12 && a >0){ 
+    console.log('Hello World!')
+    a--
+    }
+
+
