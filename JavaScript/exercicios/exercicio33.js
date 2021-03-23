0bc6d0a3ddf4a71d9beb7709c5005b561ae26d18
Pedro Henrique Paes <phpaes252@gmail.com>
2021-03-23 11:51:36 -0300

    :rocket:

---
+++ JavaScript/exercicios/exercicio33.js
@@ -0,0 +1,18 @@
+/*33) Crie três vetores, chamados vetorInteiro, vetorString e vetorDouble. Cada um destes vetores deverá conter
+quatro valores, sendo o primeiro com valores inteiros, o segundo com strings e o terceiro com valores decimais.
+Declarados os vetores, utilize a função de união concat() de duas maneiras diferentes para unir os vetores, e
+mostre o resultado no console. Todos os elementos do vetor resultado deverão aparecer no console.*/
+let uniao = (...arguments) =>{
+    concatenar = []
+    for(let i=0; i<arguments.length; i++){
+        concatenar = concatenar.concat(arguments[i])
+    }
+    return concatenar
+}
+let vetorInteiro = [1, 2, 3, 4]
+let vetorString = ['Oi','Olá','Ok','Show']
+let vetorDouble = [0.1, 0.2, 0.3, 0.4]
+
+
+console.log(uniao(vetorString, vetorInteiro))
+console.log(uniao(vetorDouble, vetorString))
